6bd0147b7a6680670dd9dd52f05036039e70da42
Silverio Dos Santos <silveriojannard@gmail.com>
2022-12-10 20:17:49 -0300

    api fake com um arquivo js

---
+++ fake-api/produto.js
@@ -0,0 +1,7 @@
+[
+    {"nome":"Fone Apple","img":"https://silveriosantos.github.io/prod-crud/fake-api/img/ir-pods.htm","valor":30.2},
+    {"nome":"Computador Apple","img":"https://silveriosantos.github.io/prod-crud/fake-api/img/iMac.htm","valor":40},
+    {"nome":"Celular Apple","img":"https://silveriosantos.github.io/prod-crud/fake-api/img/iphone.htm","valor":50.2},
+    {"nome":"Celular Samsung","img":"https://silveriosantos.github.io/prod-crud/fake-api/img/s22-ultra.htm","valor":22.22},
+    {"nome":"Conseole Microsoft","img":"https://silveriosantos.github.io/prod-crud/fake-api/img/x-box-x.htm","valor":40.50},
+]
